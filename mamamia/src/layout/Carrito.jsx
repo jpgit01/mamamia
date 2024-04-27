bfd068c82bca63ad8b7375a67b7d85effb257a81
Juan Pablo M. <pablochileidh@gmail.com>
2024-04-27 08:53:27 -0400

    ajuste carrito

--- mamamia/src/layout/Carrito.jsx
+++ mamamia/src/layout/Carrito.jsx
@@ -9,10 +9,10 @@ import Image from "react-bootstrap/Image";
 import "./Carrito.css";
 
 const Carrito = () => {
-  const { productosSeleccionados, totalPrecio, ajustarCantidad } = useContext(MiContexto);
+  const { productosSeleccionados, totalPrecio, ajustarCantidad } =
+    useContext(MiContexto);
 
-  useEffect(() => {
-  }, [productosSeleccionados]);
+  useEffect(() => {}, [productosSeleccionados]);
 
   return (
     <Container>
@@ -20,29 +20,48 @@ const Carrito = () => {
         <Col className="text-center">
           <h3 className="mt-2 mb-2">Productos en el carrito:</h3>
         </Col>
-        
-        <Table striped bordered hover>
-          <tbody>
-            {productosSeleccionados.map((producto, index) => (
-              <tr key={index}>
-                <td>
+      </Row>
+      <Row>
+        <Col>
+          {productosSeleccionados.map((producto, index) => (
+            <Row key={index}>
+              <div className="contenidob">
+                <div className="contenido">
                   <Image className="minim" src={producto.img} />
-                </td>
-                <td>{producto.name}</td>
-                <td>$ {producto.price * producto.cantidad}</td>
-                <td>
-                  <Button onClick={() => ajustarCantidad(index, 1)}>+</Button>
-                  {producto.cantidad}
-                  <Button onClick={() => ajustarCantidad(index, -1)}>-</Button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+                  <h5 className="text-capitalize ms-3"> {producto.name}</h5>
+                </div>
+
+                <div className=" contenido text-end">
+                  <div><h5 className="me-3">$ {producto.price * producto.cantidad}</h5></div>
+                  <div>
+                    <Button
+                      className="mr-5"
+                      onClick={() => ajustarCantidad(index, 1)}
+                    >
+                      +
+                    </Button>
+                  </div>
+                  <div className="m-3">
+                    <h5>{producto.cantidad}</h5>
+                  </div>
+                  <div>
+                    <Button
+                      variant="danger"
+                      onClick={() => ajustarCantidad(index, -1)}
+                    >
+                      -
+                    </Button>
+                  </div>
+                </div>
+              </div>
+            </Row>
+          ))}
+        </Col>
       </Row>
       <Row>
         <Col className="text-center">
-          <h4>Total: ${totalPrecio}</h4>
+          <h3>Total: ${totalPrecio}</h3>
+          <Button variant="success">Ir a pagar</Button>
         </Col>
       </Row>
     </Container>
